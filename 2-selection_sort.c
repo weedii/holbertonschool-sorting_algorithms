cae8e116a365e19c6c9cbe58a62e04567fa4918c
Aminekham <lkham0508@gmail.com>
2022-12-23 23:46:10 +0100

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,29 @@
 #include "sort.h"
 /**
- * 
- * 
-*/
+ *selection sort - the selection sorting function
+ *@array: the array to sort
+ *@size: the size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int swap;
-    
-    for (i = 0; i < size; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-            {
-                min = j;
-            }
-        }
-        if (min != i)
-        {
-            swap = array[i];
-            array[i] = array[min];
-            array[min] = swap;
-        }
-    }
+	size_t i, j, min;
+	int swap;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			swap = array[i];
+			array[i] = array[min];
+			array[min] = swap;
+		}
+	}
 }
